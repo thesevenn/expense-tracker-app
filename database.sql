80730f0df0bbfc575e96e59ca4eb301901c97eb8
ScriptD <adironaldo1234@gmail.com>
2023-03-20 13:02:52 +0530

    made slight changes to folder sturcrure and renamed some base files

--- database.sql
+++ database.sql
@@ -1,9 +1,52 @@
-CREATE DATABASE EXPENSES;
- CREATE TABLE users_table 
+CREATE DATABASE expenses;
+
+ CREATE TABLE users 
     (
-        u_id VARCHAR(100) FOREIGN KEY PRIMARY KEY UNIQUE,
+        id VARCHAR(100) PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         store BOOLEAN,
-        joined_at TIMESTAMP NOt NULL
+        joined_at TIMESTAMP
     )
+
+    CREATE TABLE expense(
+        id VARCHAR(255) PRIMARY KEY,
+        amount NUMERIC(10,3),
+        credit BOOLEAN DEFAULT FALSE,
+        u_id FOREIGN KEY,
+        date DATE DEFAULT DATE,
+        time TIMESTAMP,
+        FOREIGN KEY (u_id) REFERENCES users(u_id)
+    )
+
+    CREATE TABLE summary(
+    id VARCHAR(50) PRIMARY KEY,
+	expense NUMERIC(10,3),
+	earn NUMERIC(10,3),
+	u_id VARCHAR(50),
+	CONSTRAINT fk_user
+	FOREIGN KEY(u_id)
+	REFERENCES users(id)
+    )
+
+    CREATE TABLE month(
+        month_name VARCHAR(40),
+        month_id SERIAL SMALLINT,
+        expense NUMERIC(10,3),
+        earn NUMERIC(10,3),
+        avg NUMERIC(10,3),
+        u_id VARCHAR(100),
+        CONSTRAINT fk_user
+      FOREIGN KEY(u_id) 
+	  REFERENCES users(id)
+    )
+
+    CREATE TABLE year(
+        year_name VARCHAR(40),
+        year_id SERIAL BIGINT,
+        expense NUMERIC(10,3),
+        earn NUMERIC(10,3),
+        avg NUMERIC(10,3),
+        u_id FOREIGN KEY
+    )
+ 
  
--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import {env} from "./constants/_env";
 
 app.listen(env.PORT, (): void => {
-	console.log("Listening at port number: " + env.PORT);
+	console.log("Server running At url:http://localhost:" + env.PORT);
 });
